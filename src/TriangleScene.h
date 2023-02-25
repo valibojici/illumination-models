c1eac14183464a4ce791c5095ded7dcce9c657ff
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-02-25 14:57:26 +0200

    add vao vbo shader member variables

--- src/TriangleScene.h
+++ src/TriangleScene.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "Scene.h"
 #include "SceneMenu.h"
+#include "Shader.h"
+#include "VBO.h"
+#include "VAO.h"
 
 class TriangleScene : public Scene
 {
 private:
-	bool show_demo_window = false;
-	bool show_another_window = false;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	glm::vec3 m_color = { 1.0f, 0.0f, 0.0f };
+
+	VBO m_vbo;
+	VAO m_vao;
+	Shader m_shader;
 public:
-	TriangleScene(Scene*& scene) : Scene(scene) {}
+	TriangleScene(Scene*& scene);
+	~TriangleScene();
 	void onRender() override;
 	void onRenderImGui() override;
 };
