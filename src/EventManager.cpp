a3d439c919a7e64c71c260f68c339e0437e2fca8
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-03-03 16:29:10 +0200

    register & implement new callbacks

--- src/EventManager.cpp
+++ src/EventManager.cpp
@@ -11,6 +11,9 @@ void EventManager::registerCallbacks(GLFWwindow* window)
 	printf("Registering callbacks...\n");
 	// TODO: add other callbacks here
 	glfwSetWindowSizeCallback(window, EventManager::handleResize);
+	glfwSetKeyCallback(window, EventManager::handleKey);
+	glfwSetCursorPosCallback(window, EventManager::handleMousePosition);
+	glfwSetMouseButtonCallback(window, EventManager::handleMouseButton);
 }
 
 void EventManager::addHandler(EventHandler* handler)
@@ -40,5 +43,39 @@ void EventManager::handleResize(GLFWwindow* window, int width, int height)
 	Event e;
 	e.m_type = Event::Type::WINDOW_RESIZE;
 	e.window = { width, height };
+	getInstance().sendEvent(e);
+}
+
+void EventManager::handleKey(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	Event e;
+	if (action == GLFW_PRESS) {
+		e.m_type = Event::Type::KEY_PRESS;
+	}
+	else if (action == GLFW_RELEASE) {
+		e.m_type = Event::Type::KEY_RELEASE;
+	}
+	e.key = { key };
+	getInstance().sendEvent(e);
+}
+
+void EventManager::handleMousePosition(GLFWwindow* window, double xpos, double ypos)
+{
+	Event e;
+	e.m_type = Event::Type::MOUSE_MOVE;
+	e.mouse = { xpos, ypos };
+	getInstance().sendEvent(e);
+}
+
+void EventManager::handleMouseButton(GLFWwindow* window, int button, int action, int mods)
+{
+	Event e;
+	if (action == GLFW_PRESS) {
+		e.m_type = Event::Type::KEY_PRESS;
+	}
+	else if (action == GLFW_RELEASE) {
+		e.m_type = Event::Type::KEY_RELEASE;
+	}
+	e.key = { button };
 	getInstance().sendEvent(e);
 }
