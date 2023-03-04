1beff9c9785eda15dc96920c7b11b1ee4ef2ffa0
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-03-04 12:56:13 +0200

    add camera member

--- src/Simple3dScene.h
+++ src/Simple3dScene.h
@@ -6,6 +6,9 @@
 #include "VAO.h"
 #include "EBO.h"
 #include "glm/gtc/matrix_transform.hpp"
+#include "GLFW/glfw3.h" // for deltaTime(glfwGetTime) in rendering
+#include "Camera.h"
+#include "EventManager.h" // for registering camera
 
 class Simple3dScene : public Scene
 {
@@ -15,6 +18,7 @@ private:
 	VBO m_vbo;
 	VAO m_vao;
 	EBO m_ebo;
+	Camera m_camera;
 	Shader m_shader;
 	glm::mat4 m_modelMatrix = glm::mat4(1.0f);
 	glm::mat4 m_viewMatrix = glm::mat4(1.0f);
