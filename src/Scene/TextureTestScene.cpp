bcfee8bc2f743f3310e75316fe02feb102e8a898
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-03-12 14:08:51 +0200

    refactor to showcase basic normal mapping

--- src/Scene/TextureTestScene.cpp
+++ src/Scene/TextureTestScene.cpp
@@ -15,7 +15,7 @@ TextureTestScene::TextureTestScene(Scene*& scene)
     m_shader.setVec3("u_lightPos", m_lightPos);
     EventManager::getInstance().addHandler(&m_camera);
     glEnable(GL_CULL_FACE);
-
+    glEnable(GL_DEPTH_TEST);
     m_textures.push_back(new Texture("textures/brickwall.png"));
     m_textures.push_back(new Texture("textures/brickwall.png", Texture::Type::SPECULAR));
     m_textures.push_back(new Texture("textures/brickwall_normal.png", Texture::Type::NORMAL));
@@ -25,8 +25,10 @@ TextureTestScene::TextureTestScene(Scene*& scene)
 TextureTestScene::~TextureTestScene()
 {
     glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
     EventManager::getInstance().removeHandler(&m_camera);
     delete m_mesh;
+    delete m_lightMesh;
     for (auto t : m_textures) {
         delete t;
     }
@@ -46,12 +48,18 @@ void TextureTestScene::onRender()
     m_shader.setFloat("u_specFactor", m_specFactor); //
     m_shader.setFloat("u_shininess", m_shininess); // 
     m_shader.setVec3("u_viewPos", m_camera.getPosition());
-    m_shader.setVec3("u_viewPos", m_camera.getPosition());
-    m_shader.setInt("u_texture", 0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    m_shader.setMat3("u_transposeInverseModel", glm::transpose(glm::inverse(glm::mat3(m_modelMatrix))));
+    m_shader.setVec3("u_lightPos", m_lightModelMatrix * glm::vec4(m_lightPos, 1.0f));
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+    m_shader.setVec3("u_emission", glm::vec3(0.0f));
+    m_shader.setMat4("u_modelMatrix", m_modelMatrix);
     m_mesh->draw(m_shader);
+
+    m_shader.setVec3("u_emission", glm::vec3(1.0f));
+    m_shader.setMat4("u_modelMatrix", glm::translate(glm::vec3(m_lightModelMatrix * glm::vec4(m_lightPos, 1.0f))));
+    m_lightMesh->draw(m_shader);
 }
 
 void TextureTestScene::onRenderImGui()
@@ -70,14 +78,23 @@ void TextureTestScene::onRenderImGui()
         m_camera.setPosition({ 0.0f, 0.0f, 5.0f });
         m_camera.setTarget({ 0.0f, 0.0f, 0.0f });
     }
-    if (ImGui::DragFloat("Light position", &m_lightRotationAngle, 1.0f, 0.0f, 360.0f)) {
-        auto rotationMatrix = glm::rotate(glm::radians(m_lightRotationAngle), glm::vec3(0.0f, 1.0f, 0.0f));
-        m_shader.setVec3("u_lightPos", glm::vec4(m_lightPos, 1.0f) * rotationMatrix);
+    if (ImGui::DragFloat("Light position", &m_lightRotationAngle, 0.75f, -360.0f, 360.0f)) {
+        m_lightModelMatrix = glm::rotate(glm::radians(m_lightRotationAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+    }
+
+    static float planeRotation = 0;
+    if (ImGui::DragFloat("Plane rotation", &planeRotation, 0.75f, 0.0f, 180.0f)) {
+        m_modelMatrix = glm::rotate(glm::radians(-planeRotation), glm::vec3(1.0f, 0.0f, 0.0f));
     }
 
     static bool showWireFrames = false;
     if (ImGui::Checkbox("Show wireframe", &showWireFrames)) {
         glPolygonMode(GL_FRONT_AND_BACK, showWireFrames ? GL_LINE : GL_FILL);
+    }
+
+    static bool normalMapping = false;
+    if (ImGui::Checkbox("Enable normal mapping", &normalMapping)) {
+        m_shader.setBool("u_normalMapping", normalMapping);
     }
 
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
--- src/Scene/TextureTestScene.h
+++ src/Scene/TextureTestScene.h
@@ -15,9 +15,11 @@ class TextureTestScene : public Scene
 {
 private:
 	Mesh* m_mesh = Mesh::getPlane(4.0f, 4.0f);
+	Mesh* m_lightMesh = Mesh::getSphere(0.1f);
 	Camera m_camera;
 	Shader m_shader;
 	glm::mat4 m_modelMatrix = glm::mat4(1.0f);
+	glm::mat4 m_lightModelMatrix = glm::mat4(1.0f);
 	glm::mat4 m_viewMatrix = glm::mat4(1.0f);
 	glm::mat4 m_projMatrix = glm::mat4(1.0f);
 	std::vector<Texture*> m_textures;
@@ -28,7 +30,7 @@ private:
 	float m_specFactor = 1.0f;
 	float m_shininess = 32;
 	glm::vec3 m_specColor = glm::vec3(1.0f);
-	glm::vec3 m_lightPos = { 5.0f, 5.0f, 5.0f };
+	glm::vec3 m_lightPos = { 2.0f, 2.0f, 2.0f };
 	float m_lightRotationAngle = 0.0f;
 
 public:
