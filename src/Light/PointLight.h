9a4e49c32d40b6b8982b36c515534be43c796aea
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-04-08 16:38:23 +0300

    add point light class

---
+++ src/Light/PointLight.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Light.h"
+
+/// <summary>
+/// Class for a point light
+/// </summary>
+class PointLight : public Light
+{
+private:
+	/// <summary>
+	/// attenuation factors: constant, linear, quadratic
+	/// </summary>
+	glm::vec3 m_attenuation = glm::vec3(1.0f, 0.1f, 0.04f);
+public:
+	/// <summary>
+	/// Constructs a new point light derived from Light class
+	/// </summary>
+	/// <param name="index">: index of light in the *shader uniform array*</param>
+	/// <param name="position">: position of the light</param>
+	PointLight(int index, const glm::vec3& position);
+
+	/// <summary>
+	/// Draw UI in ImGui, sets uniforms on value change.
+	/// </summary>
+	void imGuiRender(Shader& shader) override;
+
+	/// <summary>
+	/// Sets all uniforms for this light (at m_index)
+	/// </summary>
+	void setUniforms(Shader& shader) override;
+};
+
