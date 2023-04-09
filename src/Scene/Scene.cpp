4bfa06bc9a665049d87541401e87cf1ce8ea18d0
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-04-09 17:55:48 +0300

    add helpPopout helper method (UI)

---
+++ src/Scene/Scene.cpp
@@ -0,0 +1,13 @@
+#include "Scene.h"
+
+void Scene::helpPoput(const char* text)
+{
+    ImGui::TextDisabled("(?)");
+    if (ImGui::IsItemHovered()) {
+        ImGui::BeginTooltip();
+        ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+        ImGui::TextUnformatted(text);
+        ImGui::PopTextWrapPos();
+        ImGui::EndTooltip();
+    }
+}
