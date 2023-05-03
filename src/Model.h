b75f7031bf446dab497cf07bdedfee47bbc05b3f
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-05-03 13:02:11 +0300

    use unique ptr

--- src/Model.h
+++ src/Model.h
@@ -11,10 +11,10 @@ class Model
 {
 private:
 	std::string m_path;
-	std::vector<std::shared_ptr<Mesh>> m_meshes;
+	std::vector<std::unique_ptr<Mesh>> m_meshes;
 
 	void processNode(const aiScene* scene, const aiNode* node);
-	std::shared_ptr<Mesh> processMesh(const aiScene* scene, const aiMesh* mesh);
+	std::unique_ptr<Mesh> processMesh(const aiScene* scene, const aiMesh* mesh);
 public:
 	void load(const std::string& path);
 	void draw(Shader& shader) const;
