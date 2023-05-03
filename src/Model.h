08ff861d0b479938027e237a3467289874a1663e
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-05-03 21:34:41 +0300

    add model matrix attribute

--- src/Model.h
+++ src/Model.h
@@ -16,6 +16,7 @@ private:
 	void processNode(const aiScene* scene, const aiNode* node);
 	std::unique_ptr<Mesh> processMesh(const aiScene* scene, const aiMesh* mesh);
 public:
+	glm::mat4 m_modelMatrix = glm::mat4(1.0f);
 	void load(const std::string& path);
 	void draw(Shader& shader) const;
 };
