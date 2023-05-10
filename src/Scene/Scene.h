d8fbce5d0433ca1f852c8965eef94ff13852fad6
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-05-10 19:54:38 +0300

    add width / height

--- src/Scene/Scene.h
+++ src/Scene/Scene.h
@@ -7,12 +7,16 @@ class Scene
 {
 protected:
 	Scene*& m_currentScene;
+
+	unsigned int m_width = 0;
+	unsigned int m_height = 0;
 public:
-	Scene(Scene*& scene) : m_currentScene(scene) {}
+	Scene(Scene*& scene, unsigned int width, unsigned int height) : m_currentScene(scene), m_width(width), m_height(height) {}
 	virtual void onRender() {}
 	// TODO: add back button rendering here
 	virtual void onRenderImGui() {}
 	virtual void onUpdate() {}
+	virtual void updateWidthHeight(unsigned int width, unsigned int height) {}
 	virtual ~Scene() {}
 	static void helpPoput(const char* text);
 };
